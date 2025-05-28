446defcc53f9bb26f99efa8b1b51ed24ea0354c3
root <root@ip-172-31-1-53.ap-south-1.compute.internal>
2025-05-28 19:21:04 +0000

    Initial Terraform code for ECS with ALB

---
+++ ecs.tf
@@ -0,0 +1,42 @@
+resource "aws_ecs_cluster" "main" {
+  name = "main-cluster"
+}
+
+resource "aws_ecs_task_definition" "task" {
+  family                   = "sample-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "nginx"
+      portMappings = [{ containerPort = 80 }]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "sample-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.private[*].id
+    security_groups = [aws_security_group.lb_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "app"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
+
